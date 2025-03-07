411132453a11fcac000cbde4f994e535ccd5e941
Prathvihan1008 <prathvihan1008@gmail.com>
2025-03-07 23:32:52 +0530

    back-end:fix(deletion of account) bug resolved for the image deletion from cloudinary

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import { Comment } from "./comment.model.js";
+import { deleteImage } from "../utils/cloudnary.js";
 
 const postSchema = new mongoose.Schema(
   {
@@ -33,8 +34,32 @@ postSchema.pre(
   "deleteOne",
   { document: true, query: false },
   async function (next) {
-    console.log(`Deleting comments for post: ${this._id}`);
+    console.log(`Deleting comments and image for post: ${this._id}`);
+
+    // Delete Comments
     await Comment.deleteMany({ post: this._id });
+
+    // Delete Post Image if Exists
+    await deleteImage(this.image);
+
+    next();
+  }
+);
+
+postSchema.pre(
+  "deleteMany",
+  { document: false, query: true },
+  async function (next) {
+    const posts = await this.model.find(this.getFilter());
+
+    for (const post of posts) {
+      console.log(`Deleting comments and image for post: ${post._id}`);
+
+      await Comment.deleteMany({ post: post._id });
+
+      await deleteImage(post.image);
+    }
+
     next();
   }
 );
