0fe392f6f88cb0c70348771cf99c4fc9360b61e0
Prathvihan1008 <prathvihan1008@gmail.com>
2025-03-10 23:24:39 +0530

    back-end:front-end:shifted from http to https production level testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,31 +1,27 @@
 import dotenv from "dotenv";
+import https from "https";
+import fs from "fs";
 import { app } from "./app.js"; // Import `app` from `app.js`
 import connectDB from "./db/db.js";
 import os from "os";
 dotenv.config({ path: "../.env" });
 
+// Load SSL certificate
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
 // console.log(`PORT from env: ${process.env.PORT}`);
 const port = process.env.PORT || 5000;
-const host_url = process.env.HOST_URL || "http://localhost";
+const host_url = process.env.HOST_URL || "https://localhost";
 console.log(`Using port: ${port}`);
 
 connectDB()
   .then(() => {
-    const server = app.listen(port, "0.0.0.0", () => {
+    const server = https.createServer(options, app).listen(port, () => {
       console.log(`🚀 Server running on ${host_url}:${port}`);
     });
-
-    function getLocalIP() {
-      const interfaces = os.networkInterfaces();
-      for (const iface of Object.values(interfaces)) {
-        for (const config of iface) {
-          if (config.family === "IPv4" && !config.internal) {
-            return config.address;
-          }
-        }
-      }
-      return "127.0.0.1";
-    }
   })
   .catch((err) => {
     console.log("❌ MongoDB Connection Failed!", err);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 
 export default defineConfig(({ mode }) => {
 	// Load environment variables based on the mode (development, production, etc.)
@@ -8,8 +9,13 @@ export default defineConfig(({ mode }) => {
 
 	return {
 		plugins: [react(), tailwindcss()],
+
 		server: {
-			host: env.VITE_HOST || "localhost", // Example usage
+			https: {
+				key: fs.readFileSync("key.pem"),
+				cert: fs.readFileSync("cert.pem"),
+			},
+			host: env.VITE_HOST || "localhost",
 			port: env.VITE_PORT || 5173,
 		},
 	};
